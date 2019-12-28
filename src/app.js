1e8de574cf08d7aed0c8e196a00bc72e070fafd5
gusmcnair <56044029+gusmcnair@users.noreply.github.com>
2019-12-28 11:00:55 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -16,19 +16,6 @@ app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
 
-app.use(function validateBearerToken(req, res, next) {
-  const apiToken = process.env.API_TOKEN
-  const authToken = req.get('Authorization')
-
-  if(!authToken || authToken.split(' ')[1] !== apiToken){
-    logger.error(`unauthorized request to path ${req.path}`);
-    return res.status(401).json({ error: 'unauthorized request'})
-  }
-  next()
-})
-
-app.use(bookmarksrouter)
-
 
    app.use(function errorHandler(error, req, res, next) {
        let response
