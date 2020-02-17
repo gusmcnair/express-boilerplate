9b889d17ca0b99e7a62fa84ac82e8063037eb2e9
gusmcnair <56044029+gusmcnair@users.noreply.github.com>
2020-02-16 19:48:30 -0600

    Update app.spec.js

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,6 +1,21 @@
 const app = require('../src/app')
 
 describe('App', () => {
+  
+    before('make knex instance', () => {
+    db = knex({
+      client: 'pg',
+      connection: process.env.TEST_DB_URL,
+    })
+    app.set('db', db)
+  })
+  
+  before('cleanup', () => db.raw('TRUNCATE TABLE todo RESTART IDENTITY;'));
+
+  afterEach('cleanup', () => db.raw('TRUNCATE TABLE todo RESTART IDENTITY;')); 
+
+  after('disconnect from the database', () => db.destroy()); 
+  
   it('GET / responds with 200 containing "Hello, world!"', () => {
     return supertest(app)
       .get('/')
